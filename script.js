45f19953418865a95d46658185041617799284fa
Matt Lubel <matt_lubel@hotmail.com>
2016-11-28 23:56:53 +0000

    overhaul. Now my personal site.

--- script.js
+++ script.js
@@ -1,5 +1,106 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     
+    // animation constants
+    var FPS = 10;
+    var ANIMATION_DURATION = 200;
+    
+    // copied from SO, good method to get height of document which is more consistent 
+    // across browsers
+    var body = document.body,
+        html = document.documentElement;
+    var bodyHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    var windowHeight = window.innerHeight;
+    
+    // navbar fixing and auto-scrolling logic
+    var nav = document.getElementById("navbar");
+    var navGhost = document.getElementById("navbar-ghost");
+    var navOffsetTop = nav.offsetTop;
 
+    function fixTopNavbar(pos) {
+        if (pos > navOffsetTop) {
+            nav.classList.add("nav-fixed");
+            navGhost.classList.remove("hidden");
+        }
+        else {
+            nav.classList.remove("nav-fixed");
+            navGhost.classList.add("hidden");
+        }
+    };
+   
+    // from MDN, ticking variable prevents overprocessing
+    var ticking = false;
+    window.addEventListener('scroll', function(e) {
+        last_known_scroll_position = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+            // decide whether navbar needs to be fixed
+            fixTopNavbar(last_known_scroll_position);
+            ticking = false;
+            });
+        }
+        ticking = true;
+    });
+    
+    // add functionality for darkify button
+    var darkifyButton = document.getElementById("darkify-button");
+    darkifyButton.addEventListener("click", darkify);
+    var navContainer = document.getElementsByClassName("nav-container")[0];
+    var footerContainer = document.getElementsByClassName("footer-container")[0];
+    var projectsArray = document.getElementsByClassName("project");
+    
+    function darkify () {
+        document.body.classList.toggle("dark");
+        // navContainer.classList.toggle("dark");
+        footerContainer.classList.toggle("dark"); 
+        for (var i=0; i<projectsArray.length; i++) {
+            projectsArray[i].classList.toggle("dark");
+        }    
+    }
+    
+    // add functionality for clicking nav buttons
+    var navItems = document.getElementsByClassName("nav-item");
+    
+    for (var i=0; i<navItems.length; i++) {
+        var item = navItems[i];
+        item.addEventListener("click", goToSection);
+    }    
+    
+    function goToSection (event) {
+        event.preventDefault();
+        var item = this;
+        // get section name, e.g. 'about' from id='nav-about'
+        var sectionName = item.id.slice(0, item.id.indexOf("-"));
+        // have actually removed the "to top" menu item
+        if (sectionName === "totop") {
+            animateScrollTo(0, ANIMATION_DURATION);
+        }
+        else {
+            var section = document.getElementById(sectionName + "-section");
+            var sectionOffset = section.offsetTop;
+            // animate scroll to the right place on page: -50 because of height of navbar
+            animateScrollTo(sectionOffset - 60, ANIMATION_DURATION);
+        }
+    }
+    
+    function animateScrollTo (offset, time) {
+        var numFrames = Math.ceil(time/FPS),
+            refreshTime = time/numFrames,
+            counter = 0;
+        // set interval with refreshTime and function which increments the scroll towards
+        // the inputted offset
+        var intervalId = window.setInterval(function () {
+            var d = (offset - window.scrollY) / (numFrames - counter);
+            window.scrollTo(0, window.scrollY + d);
+            // clear interval when counter equals number of frames
+            if (++counter >= numFrames){
+                window.clearInterval(intervalId);
+            } 
+        }, refreshTime);
+    };
+      
+    // add logic for window resize
+    window.onresize = updateVariables;
+    function updateVariables () {   
+    };
+     
 });
